6d36c110a2e28c928b515dec775eb210dbcc99bf
Jari Honkanen <jaarhonka@yahoo.com>
2025-12-29 09:23:26 +0200

    initial commit

---
+++ fc_4level_x1.cpp
@@ -0,0 +1,90 @@
+// Automatically generated C++ file on Sun Dec 28 09:08:33 2025
+//
+// To build with Digital Mars C++ Compiler:
+//
+//    dmc -mn -WD fc_4level_x1.cpp kernel32.lib
+
+#include <cmath>
+union uData
+{
+   bool b;
+   char c;
+   unsigned char uc;
+   short s;
+   unsigned short us;
+   int i;
+   unsigned int ui;
+   float f;
+   double d;
+   long long int i64;
+   unsigned long long int ui64;
+   char *str;
+   unsigned char *bytes;
+};
+
+// int DllMain() must exist and return 1 for a process to load the .DLL
+// See https://docs.microsoft.com/en-us/windows/win32/dlls/dllmain for more information.
+int __stdcall DllMain(void *module, unsigned int reason, void *reserved) { return 1; }
+
+// #undef pin names lest they collide with names in any header file(s) you might include.
+#undef gate1
+#undef gate2
+#undef gate3
+#undef gate4
+#undef gate5
+#undef gate6
+#undef gate7
+#undef gate8
+
+double pwm = 0;
+double pwm_prev = 1;
+double dt = 0;
+
+extern "C" __declspec(dllexport) void fc_4level_x1(void **opaque, double t, union uData *data)
+{
+   double &gate1 = data[0].d; // output
+   double &gate2 = data[1].d; // output
+   double &gate3 = data[2].d; // output
+   double &gate4 = data[3].d; // output
+   double &gate5 = data[4].d; // output
+   double &gate6 = data[5].d; // output
+   double &gate7 = data[6].d; // output
+   double &gate8 = data[7].d; // output
+
+// Implement module evaluation code here:
+
+   double carrier = std::fmod(t, 1e-6)*1e6;
+   const double duty = 0.5;
+
+   if ((carrier > (0.5-duty/2.0)) && (carrier < (0.5+duty/2.0)))
+   {
+        pwm = 5;
+
+   } else
+   {
+        pwm = 0;
+
+   }
+      gate1 = pwm;
+      gate2 = pwm;
+      gate3 = pwm;
+      gate4 = pwm;
+      gate5 = 5-pwm;
+      gate6 = 5-pwm;
+      gate7 = 5-pwm;
+      gate8 = 5-pwm;
+
+    if (pwm_prev != pwm)
+    {
+      gate1 = 0;
+      gate2 = 0;
+      gate3 = 0;
+      gate4 = 0;
+      gate5 = 0;
+      gate6 = 0;
+      gate7 = 0;
+      gate8 = 0;
+    }
+   pwm_prev = pwm;
+
+}
